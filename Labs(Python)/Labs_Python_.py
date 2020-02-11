d3c35afe5ca29c63a122ffe3529c2f6a15b73bd2
khmelinin <khmelinin2001@gmail.com>
2020-02-11 23:41:28 +0200

    1

--- Labs(Python)/Labs_Python_.py
+++ Labs(Python)/Labs_Python_.py
@@ -1,1 +1,11 @@
-
+import random as r
+n=100
+list=[]
+for i in range(n):
+    list.append(round(r.randint(-n,n),2))
+print(list)
+for i in range(n-1):
+    for j in range(n-1):
+        if list[j]>list[j+1]:
+            list[j],list[j+1]=list[j+1],list[j]
+print(list)
