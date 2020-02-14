4597a25ab43bf5887e185f2554eb1a2a0cddde28
khmelinin <khmelinin2001@gmail.com>
2020-02-14 09:56:11 +0200

    bubble sort finally

--- Labs(Python)/Labs_Python_.py
+++ Labs(Python)/Labs_Python_.py
@@ -3,55 +3,69 @@ n=10
 list=[]
 worst=[]
 best=[]
+bestT=[]
 
 for i in range(n):
     best.append(i+1)
 print("Best before")
 print(best)
+bestT=best
 print()
 countB=0
 
 for i in range(n-1):
     for j in range(n-1):
+        countB+=1
         if best[j]>best[j+1]:
             best[j],best[j+1]=best[j+1],best[j]
-            countB+=1
+        
+    if best==bestT:
+        break
+
 print("Best after")
 print(best)
 print()
-print(countB)
-
+print(countB,"\n","--------------------")
+#------------------------
 for i in range(n):
     list.append(i+1)
 r.shuffle(list)
 print("Random before")
 print(list)
+listT=list
 print()
 count=0
 
 for i in range(n-1):
     for j in range(n-1):
+        count+=1
         if list[j]>list[j+1]:
             list[j],list[j+1]=list[j+1],list[j]
-            count+=1
+        
+    if list==bestT:
+        break
 print("Random after")
 print(list)
 print()
-print(count)
+print(count,"\n","--------------------")
 #------------------------
 for i in range(n):
     worst.append(10-i)
 print("Worst before")
 print(worst)
+worstT=worst
 print()
 countW=0
 
 for i in range(n-1):
     for j in range(n-1):
+        countW+=1
         if worst[j]>worst[j+1]:
             worst[j],worst[j+1]=worst[j+1],worst[j]
-            countW+=1
+        
+    if worst==bestT:
+        break
 print("Worst after")
 print(worst)
 print()
-print(countW)
+print(countW,"\n","--------------------")
