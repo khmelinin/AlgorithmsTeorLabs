6e779fe96bc638ae286b7cf0484094a612f584dc
Hard <Khmelinin2001@gmail.com>
2020-03-07 20:28:35 +0200

    lab 3(line 40 in for (int))

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import random as r
 import operator
+import time
 #-------------merge--------------------------------------------
 def merge(left, right, compare):
     result = []
@@ -29,7 +29,7 @@ def merge_sort(L, compare=operator.lt):
         middle = int(len(L) / 2)
         left = merge_sort(L[:middle], compare)
         right = merge_sort(L[middle:], compare)
-        return merge(left, right, compare)
+        merge(left, right, compare)
 
 #-------------merge--------------------------------------------
 
@@ -37,108 +37,79 @@ def merge_sort(L, compare=operator.lt):
 def insertion_sort(nums):  
     nums1=nums[:]
     countt=0
-    for i in range(1, len(nums)):
+    leng=len(nums)
+    for i in range(1, leng):
         item_to_insert = nums1[i]
         
         j = i - 1
+        #########
         countt+=1
         while j >= 0 and nums1[j] > item_to_insert:
+            #############
             countt+=1
             nums1[j + 1] = nums1[j]
             j -= 1
         
         nums1[j + 1] = item_to_insert
-    return countt
 #--------------insertion------------------------------------------
 
 
 
 
+from random import shuffle
+
+# def compare_ins_and_merge()
+# def test(f,data):
+def best_case(n):
+    i=0
+    arr = [i for i in range(i, n + 1)]
+    return arr
 
 
-n1=list(range(10,110,10))
-
-count=[]
-#count2=[]
-count3=[]
-countB=[]
-#countB2=[]
-countB3=[]
-countW=[]
-#countW2=[]
-countW3=[]
-for n in n1:
+def random_case(n):
+    i=0
+    arr = [i for i in range(i, n + 1)]
+    shuffle(arr)
+    return arr
 
 
-    best=list(range(1,n+1))
+def worst_case(n):
+    i=0
+    arr = [i for i in range(i, n + 1)]
+    arr = list(reversed(arr))
+    return arr
 
-    #print("Best before")
-    print(best,"\n")
-
-
-
-    list1=best[:]
-    r.shuffle(list1)
-    print("Random before")
-    print(list,"\n")
+    
 
 
-    worst=list(range(n,0,-1))
-
-    print("Worst before")
-    print(worst,"\n\n")
-
-
-    #print("Bubble")
-    #print("\nBest after counter = ",a)
-    #print(countB)
-    #print("\nRandom after counter = ",b)
-    #print(count)
-    #print("\nWorst after counter = ",c)
-    #print(countW)
-    #print("\n____________________________")
+def test(f,data):
+    repeats=3
+    start = time.perf_counter()
+    for i in range (repeats):
+        f(data[i])
+    end = time.perf_counter()
+    return (end-start)/repeats
 
 
-    #print("Better_Bubble")
-    #print("\nBest after counter = ",a)
-    #print(countB2)
-    #print("\nRandom after counter = ",b)
-    #print(count2)
-    #print("\nWorst after counter = ",c)
-    #print(countW2)
-    #print("\n____________________________")
+len = 10
+arr1 = best_case(len);
+arr3 = worst_case(len);
+arr2 = random_case(len);
+data = [arr1, arr2, arr3]
 
-    merge_sort(best)
+value = test(insertion_sort, data);
 
-    countB3.append(insertion_sort(best))
-
-    count3.append(insertion_sort(list1))
-
-    countW3.append(insertion_sort(worst))
-
-    #print("Insertion")
-    #print("\nBest after counter = ",a)
-    #print(countB3)
-    #print("\nRandom after counter = ",b)
-    #print(count3)
-    #print("\nWorst after counter = ",c)
-    #print(countW3)
-    #print("\n____________________________")
-
-
-
-
-plt.plot(#n1,count, "--y",
+#plt.plot(#n1,count, "--y",
          #n1,count2, "--k",
-         n1,count3, "--r",
+         #n1,count3, "--r",
          #n1, countB, ":y",
          #n1, countB2, ":k",
-         n1, countB3, ":r",
+         #n1, countB3, ":r",
          #n1, countW, "y",
          #n1, countW2, "k",
-         n1, countW3, "r"
-         )
-plt.xlabel("length of list")
-plt.ylabel("counter")
-plt.legend(("random case bubble_sort","random case better_bubble_sort","random case insertion_sort","best case bubble_sort", "best case better_bubble_sort","best case insertion_sort","worst case bubble_sort","worst case better_bubble_sort","worst case insertion_sort"))
-plt.show()
+         #n1, countW3, "r"
+         #)
+#plt.xlabel("length of list")
+#plt.ylabel("counter")
+#plt.legend(("random case bubble_sort","random case better_bubble_sort","random case insertion_sort","best case bubble_sort", "best case better_bubble_sort","best case insertion_sort","worst case bubble_sort","worst case better_bubble_sort","worst case insertion_sort"))
+#plt.show()
