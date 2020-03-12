692934dc52800a42c5dfb0f857ee9fb47e1384f3
Hard <Khmelinin2001@gmail.com>
2020-03-13 00:55:48 +0200

    eh

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,115 +1,116 @@
 import matplotlib.pyplot as plt
-import os
 import random as r
-import operator
+import copy
 import time
 #-------------merge--------------------------------------------
-def merge(left, right, compare):
-    result = []
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-        if compare(left[i], right[j]):
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    while i < len(left):
-        result.append(left[i])
-        i += 1
-    while j < len(right):
-        result.append(right[j])
-        j += 1
-    return result
 
-def merge_sort(L, compare=operator.lt):
+def merge_sort(L,begin,end):
     if len(L) < 2:
         return L[:]
     else:
-        middle = int(len(L) / 2)
-        left = merge_sort(L[:middle], compare)
-        right = merge_sort(L[middle:], compare)
-        merge(left, right, compare)
+        middle = int((end-begin+1)/2+begin)
+        left = merge_sort(L[begin:middle],0,len(L[begin:middle])-1)
+        right = merge_sort(L[middle:end+1],0,len(L[middle:end+1])-1)
+        result = []
+        i, j = 0, 0
+        while i < len(left) and j < len(right):
+            if left[i]<right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        while i < len(left):
+            result.append(left[i])
+            i += 1
+        while j < len(right):
+            result.append(right[j])
+            j += 1
+        return result
 
 #-------------merge--------------------------------------------
 
 #--------------insertion------------------------------------------
-def insertion_sort(nums):  
+def insertion_sort(nums,begin,end):  
     nums1=nums[:]
     countt=0
-    leng=len(nums)
-    for i in range(1, leng):
+    for i in range(begin+1, end+1):
+
         item_to_insert = nums1[i]
         
         j = i - 1
-        #########
         countt+=1
         while j >= 0 and nums1[j] > item_to_insert:
-            #############
             countt+=1
             nums1[j + 1] = nums1[j]
             j -= 1
         
         nums1[j + 1] = item_to_insert
+    return countt
 #--------------insertion------------------------------------------
-
-
-
-
-from random import shuffle
-
-# def compare_ins_and_merge()
-# def test(f,data):
-def best_case(n):
-    i=0
-    arr = [i for i in range(i, n + 1)]
-    return arr
-
-
-def random_case(n):
-    i=0
-    arr = [i for i in range(i, n + 1)]
-    shuffle(arr)
-    return arr
-
-
-def worst_case(n):
-    i=0
-    arr = [i for i in range(i, n + 1)]
-    arr = list(reversed(arr))
-    return arr
-
-    
-
-
 def test(f,data):
-    repeats=3
+    repeats1 = len(data)
+
     start = time.perf_counter()
-    for i in range (repeats):
-        f(data[i])
+
+    for i in range(repeats1):
+
+        # тут можна вивести поточну вхідну послідовність для сортування
+
+        #print (data[i])
+
+        f(data[i], 0, len(data[i])-1)
+
+        # тут можна вивести поточну відсортовану послідовність
+
+        #print data[i], '\n'
+
     end = time.perf_counter()
-    return (end-start)/repeats
+
+    print((end-start)/repeats1, '\n')
+
+    return (end-start)/repeats1
+
+def compare_ins_and_merge():
+     # параметри для проведення експерименту
+    repeats = 1000    # кількість запусків для однієї розмірності
+    n_begin = 1        # початкова розмірність задачі
+    n_end   = 200    # кінцева розмірність задачі
+    n_step  = 5        # крок розмірності
+
+    t_ins, t_mer,t_comp=[],[],[]
+    for n in range(n_begin,n_end+1,n_step):
+
+        data = [generate_data(n) for i in range(repeats)]
+        a=test(insertion_sort, copy.deepcopy(data))
+        t_ins.append(a)
+        b=test(merge_sort,copy.deepcopy(data))
+        t_mer.append(b)
+        c= a/b
+        t_comp.append(c)
+        print( "\nDATA SIZE:",n)
+        print("Merge time for size", n, ":", b)
+        print("Insertion time for size", n, ":", a)
+        print("Ratio insertion/merge:", c)
+    x=list(range(n_begin,n_end+1,n_step))
+    plt.plot(x,t_ins,"k",
+             x,t_mer,"r")
+    plt.xlabel("length of list")
+    plt.ylabel("time")
+    plt.legend(("insertion_sort","merge_sort"))
+    plt.show()
+    plt.plot(x,t_comp,"k",
+             x,[1]*len(x),"r")
+    plt.xlabel("length of list")
+    plt.ylabel("insertion time / merge time")
+    
+    plt.show()
 
 
-len = 10
-arr1 = best_case(len);
-arr3 = worst_case(len);
-arr2 = random_case(len);
-data = [arr1, arr2, arr3]
+def generate_data(kk):
+    data123=list(range(kk))
+    r.shuffle(data123)
+    return data123
 
-value = test(insertion_sort, data);
 
-#plt.plot(#n1,count, "--y",
-         #n1,count2, "--k",
-         #n1,count3, "--r",
-         #n1, countB, ":y",
-         #n1, countB2, ":k",
-         #n1, countB3, ":r",
-         #n1, countW, "y",
-         #n1, countW2, "k",
-         #n1, countW3, "r"
-         #)
-#plt.xlabel("length of list")
-#plt.ylabel("counter")
-#plt.legend(("random case bubble_sort","random case better_bubble_sort","random case insertion_sort","best case bubble_sort", "best case better_bubble_sort","best case insertion_sort","worst case bubble_sort","worst case better_bubble_sort","worst case insertion_sort"))
-#plt.show()
+compare_ins_and_merge()
