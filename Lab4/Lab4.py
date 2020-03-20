40985efdbbd83c48b462834f5ddd7b2e207573ac
Hard <Khmelinin2001@gmail.com>
2020-03-20 12:04:22 +0200

    adding in lab 4 3median

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,42 +1,91 @@
-def partition(arr, low, high):
-    i = (low - 1)  # index of smaller element
-    pivot = arr[high]  # pivot
-    count=0
-    for j in range(low, high):
-        ######################################################################################
-        count+=2
-        if arr[j] <= pivot:
-            #count += 1
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
+def quick_sort(A,p,r):
 
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    #print("count",count)
-    return (i + 1),count
+    if p < r:
+        q = partition(A,p,r)
+        quick_sort(A,p,q-1)
+        quick_sort(A,q+1,r)
 
-def quickSort(arr, low, high):
-    if low < high:
-        pi,count = partition(arr, low, high)
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
-        return count
+def partition(A,p,r):
+    global count
+    x=A[r]
+    i=p-1
+    for j in range(p,r):
+        count+=1
+        if A[j]<=x:
+            i+=1
+            A[i],A[j]=A[j],A[i]
+    A[i+1],A[r]=A[r],A[i+1]
+    return i+1
 
+########## with 3 medians ###############
+def quickSort(L, ascending = True): 
+    quicksorthelp(L, 0, len(L), ascending)
+
+
+def quicksorthelp(L, low, high, ascending = True): 
+    result = 0
+    #
+    global count1
+    count1+= 1
+    #
+    if low < high: 
+        pivot_location, result = Partition(L, low, high, ascending)  
+        result += quicksorthelp(L, low, pivot_location, ascending)  
+        result += quicksorthelp(L, pivot_location + 1, high, ascending)
+    return result
+
+def Partition(L, low, high, ascending = True):
+    print('Quicksort, Parameter L:')
+    print(L)
+    result = 0 
+    pivot, pidx = median_of_three(L, low, high)
+    L[low], L[pidx] = L[pidx], L[low]
+    i = low + 1
+    for j in range(low+1, high, 1):
+        result += 1
+        if (ascending and L[j] < pivot) or (not ascending and L[j] > pivot):
+            L[i], L[j] = L[j], L[i]  
+            i += 1
+    L[low], L[i-1] = L[i-1], L[low] 
+    return i - 1, result
+
+
+def median_of_three(L, low, high):
+    mid = (low+high-1)//2
+    a = L[low]
+    b = L[mid]
+    c = L[high-1]
+    if a <= b <= c:
+        return b, mid
+    if c <= b <= a:
+        return b, mid
+    if a <= c <= b:
+        return c, high-1
+    if b <= c <= a:
+        return c, high-1
+    return a, low
+
+########## with 3 medians ###############
+
+#name = input()
 count = 0
+count1 = 0
 f = open('input_01_10.txt','r')
 l = [line.strip() for line in f]
 intl =[]
 for i in l:
     intl.append(int(i))
-#amount = intl.pop(0)
-#print(amount)
+amount = intl.pop(0)
+print(amount)
 print(intl);
+#quick_sort(intl,0,amount-1)
+quickSort(intl)
+print(count1)
+#print(intl)
+#print(count)
 
 
-s = quickSort(intl, 0, len(intl)-1)
-
-print(intl)
-print(s)
 
 f = open('Aoutput_01_10.txt','w')
-f.write(str(s) + '\n')
+f.write(str(count) + ' ')
 
